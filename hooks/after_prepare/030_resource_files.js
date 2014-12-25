0dbcfd8f07435ba9ea29da2e804874e08681e5cd
Janagaran Varadaraj <vjanagaran@gmail.com>
2014-12-26 01:54:17 +0530

    Initial Commit

---
+++ hooks/after_prepare/030_resource_files.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+var filestocopy = [{
+    "ant.properties": "platforms/android/ant.properties"
+}, ];
+
+var fs = require('fs');
+var path = require('path');
+
+// no need to configure below
+var rootdir = process.argv[2];
+
+filestocopy.forEach(function(obj) {
+    Object.keys(obj).forEach(function(key) {
+        var val = obj[key];
+        var srcfile = path.join(rootdir, key);
+        var destfile = path.join(rootdir, val);
+        //console.log("copying "+srcfile+" to "+destfile);
+        var destdir = path.dirname(destfile);
+        if (fs.existsSync(srcfile) && fs.existsSync(destdir)) {
+            fs.createReadStream(srcfile).pipe(fs.createWriteStream(destfile));
+        }
+    });
+});
